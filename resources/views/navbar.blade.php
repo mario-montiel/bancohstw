f93ed4a13367aeb379bff9201acf0ef3c2f3c8f2
egsded <sarcasmo1601@gmail.com>
2019-11-23 13:28:56 -0600

    pifi cambios

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,15 @@
+@yield('navbar')
+<div class="container-fluid container_all">
+    <nav id="nav" class="nav">
+        <div class="hamburger">
+            <div class="line line1"></div>
+            <div class="line line2"></div>
+            <div class="line line3"></div>
+        </div>
+        <ul class="nav-links animated fadeInLeft">
+            <li><a id="a1" href="#nosotros"><b>¿QUIENES SOMOS?</b></a></li>
+            <li><a id="a2" href="#servicios"><b>SERVICIOS</b></a></li>
+            <li><a id="a3" href="#contactanos"><b>CONTACTANOS</b></a></li>
+        </ul>
+    </nav>
+</div>
