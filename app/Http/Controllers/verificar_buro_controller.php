01f2969a53dda8389c87e90126f270e4e6155308
Mario-Montiel <mario159_@hotmail.com>
2019-11-30 16:02:31 -0600

    pruebon

--- app/Http/Controllers/verificar_buro_controller.php
+++ app/Http/Controllers/verificar_buro_controller.php
@@ -12,11 +12,20 @@ class verificar_buro_controller extends Controller
     }
 
     public function get_clientes(){
+
+        $datos = 1;
         // $usuarios = Usuario::where();
         $usuarios = DB::table('usuarios')
-                    ->select('clientes.cli_nom, clientes.ali_fecha_nac')
-                    ->join('clientes', 'clientes.usu_id', 'usuarios.usu_id')
+                    ->select('clientes.cliente_id')
+                    ->select('usuarios.usu_fecha_reg_bc')
+                    ->select('clientes.cli_nom')
+                    ->select('clientes.ali_fecha_nac')
+                    ->select('clientes.cli_rfc')
+                    ->join('clientes', 'clientes.usu_id', '=', 'usuarios.usu_id')
+                    ->where('usuarios.usu_id like "%'. $datos . "%'")
                     ->get();
         dd($usuarios);
+
+        // $sql = "SELECT * FROM usuarios WHERE clientes"
     }
 }
--- resources/views/scripts/scripts.blade.php
+++ resources/views/scripts/scripts.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/bancohstw.css">
+    <link rel="stylesheet" href="/css/banco_hstw.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <!-- Latest compiled and minified CSS BOOTSTRAP SELECT -->
     <link rel="stylesheet" href="css/bootstrapselect.css">
