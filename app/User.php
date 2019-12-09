052b3f55b2bd9c49484fa60f044ac737af569ace
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-12-09 17:19:24 -0600

    sdgdfgf

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function cliente()
+    {
+        return $this->belongsTo('ClientesModelo', 'cliente_id', 'id');
+    }
 }
