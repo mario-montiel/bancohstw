bffb54d6542a16264279f35745b6b2dcc64f3fa5
Mario-Montiel <mario159_@hotmail.com>
2019-12-09 19:41:11 -0600

    pruebon

--- app/Http/Controllers/verificar_buro_controller.php
+++ app/Http/Controllers/verificar_buro_controller.php
@@ -14,13 +14,13 @@ class verificar_buro_controller extends Controller
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
-                    ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
-                    ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
-                    ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
-                    ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
+                    ->leftJoin('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
+                    ->leftJoin('paises', 'paises.pais_id', '=', 'estados.pais_id')
+                    ->leftJoin('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
+                    ->leftJoin('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
+                    ->leftJoin('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
                     ->get();
-        dd($usuarios);
+        // dd($usuarios);
         return view('verificar_buro', compact('usuarios'));
     }
 
@@ -32,11 +32,11 @@ class verificar_buro_controller extends Controller
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
-                    ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
-                    ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
-                    ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
-                    ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
+                    ->leftJoin('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
+                    ->leftJoin('paises', 'paises.pais_id', '=', 'estados.pais_id')
+                    ->leftJoin('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
+                    ->leftJoin('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
+                    ->leftJoin('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
                     ->where('cli_nom', 'LIKE', '%'.$request->verificar_nom_client.'%')
                     ->orWhere('cli_curp', 'LIKE', '%'.$request->verificar_nom_client.'%')
                     ->orWhere('cli_rfc', 'LIKE', '%'.$request->verificar_nom_client.'%')
@@ -53,11 +53,11 @@ class verificar_buro_controller extends Controller
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
-                    ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
-                    ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
-                    ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
-                    ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
+                    ->leftJoin('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
+                    ->leftJoin('paises', 'paises.pais_id', '=', 'estados.pais_id')
+                    ->leftJoin('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
+                    ->leftJoin('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
+                    ->leftJoin('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
                     ->orWhere('cli_curp', 'LIKE', '%'.$request->buscar_clientes_curp.'%')
                     ->get();
 
@@ -67,15 +67,15 @@ class verificar_buro_controller extends Controller
     public function buscar_clientes_rfc(Request $request){
         $usuarios = DB::table('users')
                     ->select('*')
-                    ->join('clientes', 'clientes.user_id', '=', 'users.id')
-                    ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
-                    ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
-                    ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
-                    ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
-                    ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
-                    ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
-                    ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
+                    ->leftJoin('clientes', 'clientes.user_id', '=', 'users.id')
+                    ->leftJoin('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
+                    ->leftJoin('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
+                    ->leftJoin('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
+                    ->leftJoin('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
+                    ->leftJoin('paises', 'paises.pais_id', '=', 'estados.pais_id')
+                    ->leftJoin('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
+                    ->leftJoin('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
+                    ->leftJoin('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
                     ->orWhere('cli_rfc', 'LIKE', '%'.$request->buscar_clientes_rfc.'%')
                     ->get();
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/mdb.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="/css/banco_hstw.css">
+        <link rel="stylesheet" href="/css/bootstrap.css">
+        <link rel="stylesheet" href="css/sweetalert2.css">
+        <link rel="stylesheet" href="css/fontawesome.css">
+        <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
 
         <title>HSTW</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -22,4 +27,20 @@
     </body>
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/mdb/mdb.js') }}"></script>
+    <script src="js/bancohstw/navbar.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/sweetalert2.js"></script>
+    <script src="js/fontawesome.js"></script>
+    <script src="js/bancohstw/verificar_buro_credito.js"></script>
+    <script src="js/bancohstw/asignar_prestamos.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="js/bancohstw/tarjetas.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </html>
