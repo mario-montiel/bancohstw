48d6c3100d5b876ba8c12bca4a8d9dc17db90d3d
iovannaEscarcega10 <iovassoto@gmail.com>
2019-12-09 19:05:52 -0600

    lala

--- app/Http/Controllers/crudController.php
+++ app/Http/Controllers/crudController.php
@@ -1,15 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
 use App\Modelos\ClientesModelo;
+use App\Modelos\Usuario;
 use Illuminate\Pagination\Paginator;
-
+use DB;
 class crudController extends Controller
 {
     public function gestionar_clientes(){
-        $cli = ClientesModelo::all();
+        // $cli = ClientesModelo::all()->inner jo;
+        // $cli = ClientesModelo::with('user')->get();
+        $cli = DB::table("clientes")
+        ->select("clientes.cliente_id", "clientes.cli_nom", "clientes.user_id", "clientes.cli_ap_paterno", "clientes.cli_ap_materno","clientes.cli_fecha_nac","clientes.cli_curp","clientes.cli_rfc","users.name")
+        ->join("users","clientes.user_id", "=", "users.id")
+        ->get();
         return view('crud/insertar',compact('cli'));
     }
     
@@ -35,8 +41,9 @@ class crudController extends Controller
         
         $id = $request->get("id");
         $cli =  ClientesModelo::findOrFail($id);
+        $user = Usuario::all();
         $cli->cli_nom=$request->get("nombre");
-        $cli->user_id=$request->get("usuario");
+        $user->name=$request->get("usuario");
         $cli->cli_ap_paterno=$request->get("appaterno");
         $cli->cli_ap_materno=$request->get("apmaterno");
         $cli->cli_fecha_nac=$request->get("fnac");
@@ -46,7 +53,10 @@ class crudController extends Controller
         return redirect("/gestionar_clientes");
     }
     public function gestionar(){
-        $cli = ClientesModelo::Where('cli_status','=','amarillo')->orWhere('cli_status','=','rojo')->get();
+        $cli = DB::table("clientes")
+        ->select("clientes.cliente_id", "clientes.cli_nom", "clientes.user_id", "clientes.cli_ap_paterno", "clientes.cli_ap_materno","clientes.cli_fecha_nac","clientes.cli_curp","clientes.cli_rfc","users.name","clientes.cli_status")
+        ->join("users","clientes.user_id", "=", "users.id")
+        ->get();
         return view("gestion_de_cobranza/gestionar",compact('cli'));
     }
     
