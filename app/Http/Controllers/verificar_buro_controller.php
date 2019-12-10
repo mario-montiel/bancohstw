cc83db5730d17b9f39c0222b518524f3881fb3b0
Mario-Montiel <mario159_@hotmail.com>
2019-12-09 19:04:32 -0600

    pruebon

--- app/Http/Controllers/verificar_buro_controller.php
+++ app/Http/Controllers/verificar_buro_controller.php
@@ -8,33 +8,32 @@ use Illuminate\Http\Request;
 class verificar_buro_controller extends Controller
 {
     public function verificar_buro_credito(){
-        $usuarios = DB::table('usuarios')
+        $usuarios = DB::table('users')
                     ->select('*')
-                    ->join('clientes', 'clientes.usu_id', '=', 'usuarios.usu_id')
+                    ->join('clientes', 'clientes.user_id', '=', 'users.id')
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
                     ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
                     ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('mensajes', 'mensajes.mensaje_id', '=', 'clientes.cliente_id')
                     ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
                     ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
                     ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
                     ->get();
+        dd($usuarios);
         return view('verificar_buro', compact('usuarios'));
     }
 
 
     public function buscar_clientes(Request $request){
-        $usuario = DB::table('usuarios')
+        $usuarios = DB::table('users')
                     ->select('*')
-                    ->join('clientes', 'clientes.usu_id', '=', 'usuarios.usu_id')
+                    ->join('clientes', 'clientes.user_id', '=', 'users.id')
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
                     ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
                     ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('mensajes', 'mensajes.mensaje_id', '=', 'clientes.cliente_id')
                     ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
                     ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
                     ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
@@ -48,15 +47,14 @@ class verificar_buro_controller extends Controller
     }
 
     public function buscar_clientes_curp(Request $request){
-        $usuario = DB::table('usuarios')
+        $usuarios = DB::table('users')
                     ->select('*')
-                    ->join('clientes', 'clientes.usu_id', '=', 'usuarios.usu_id')
+                    ->join('clientes', 'clientes.user_id', '=', 'users.id')
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
                     ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
                     ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('mensajes', 'mensajes.mensaje_id', '=', 'clientes.cliente_id')
                     ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
                     ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
                     ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
@@ -67,15 +65,14 @@ class verificar_buro_controller extends Controller
     }
 
     public function buscar_clientes_rfc(Request $request){
-        $usuario = DB::table('usuarios')
+        $usuarios = DB::table('users')
                     ->select('*')
-                    ->join('clientes', 'clientes.usu_id', '=', 'usuarios.usu_id')
+                    ->join('clientes', 'clientes.user_id', '=', 'users.id')
                     ->join('direcciones_has_clientes', 'direcciones_has_clientes.clientes_cliente_id', '=', 'clientes.cliente_id')
                     ->join('direcciones', 'direcciones.direccion_id', '=', 'direcciones_has_clientes.direcciones_direccion_id')
                     ->join('ciudades', 'ciudades.ciudad_id', '=', 'direcciones.ciudad_id')
                     ->join('estados', 'estados.estado_id', '=', 'ciudades.estado_id')
                     ->join('paises', 'paises.pais_id', '=', 'estados.pais_id')
-                    ->join('mensajes', 'mensajes.mensaje_id', '=', 'clientes.cliente_id')
                     ->join('tarjetas', 'tarjetas.cliente_id', '=', 'clientes.cliente_id')
                     ->join('tipos_tarjetas', 'tipos_tarjetas.tipo_tarjeta_id', '=', 'tarjetas.tipo_tarjeta_id')
                     ->join('tipos_tarjetas_deb_cred', 'tipos_tarjetas_deb_cred.tipo_tarjeta_deb_cred_id', '=', 'tarjetas.tipo_tarjeta_deb_cred_id')
