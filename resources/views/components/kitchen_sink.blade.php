d4a2fe291b4f3f572a885e038c98c9d6a00a4301
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-12-11 17:24:56 -0600

    index y nav

--- resources/views/components/kitchen_sink.blade.php
+++ resources/views/components/kitchen_sink.blade.php
@@ -1,15 +1,11 @@
 <div class="card ml-4" style="width: 20rem;">
   <div class="card-body">
-    <h5 class="font-weight-bold mb-3">Text</h5>
-    <p class="mb-0">Some quick example text to build on the panel title and make up the bulk of the panel's content.</p>
+    <h5 class="font-weight-bold mb-3">Cuentas seguras contra</h5>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
+    <li class="list-group-item">Robos</li>
+    <li class="list-group-item">Extrabíos</li>
+    <li class="list-group-item">Falcificaciones</li>
+    <li class="list-group-item">Manipulaciones</li>
   </ul>
-  <div class="card-body">
-    <a href="#!" class="card-link">Card link</a>
-    <a href="#!" class="card-link">Another link</a>
-  </div>
 </div>
